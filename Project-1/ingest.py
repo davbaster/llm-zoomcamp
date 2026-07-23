06f241a5f4b0a22a1b8c561e1d5f7f923d989514
David <davbaster@gmail.com>
2026-07-22 21:49:35 -0600

    working in textsearch rag

--- Project-1/ingest.py
+++ Project-1/ingest.py
@@ -1,17 +1,29 @@
-import numpy as np
+import sys
+
 import pandas as pd
 from minsearch import Index
 
 def load_data():
-    df = pd.read_csv("./top_anime_dataset.csv")
-    df.head()
+    df = pd.read_csv("c:/DATA/Cursos/llm-zoomcamp/Project-1/top_anime_dataset.csv")
+    df = df.fillna("Not Available")
 
     return df.to_dict(orient="records")
 
-def build_index(documents):
+def build_index_keyword(documents):
     index = Index(
-        text_fields=["question", "section", "answer"],
-        keyword_fields=["course"]
+        text_fields=["title", "studios", "genres","type", "synopsis"],
+        keyword_fields=["mal_id"]
     )
     index.fit(documents)
     return index
+
+if __name__ == "__main__":
+    documents = load_data()
+    index = build_index_keyword(documents)
+
+    query = "I want to watch animes from Production I.G. Can you recommend one?"
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+
+    answer = index.search(query)
+    print(answer)
---
+++ Project-1/rag_helper.py
@@ -0,0 +1,83 @@
+INSTRUCTIONS = '''
+Your task is to recommend anime to users based on the provided context.
+
+Use the context to find relevant information and provide accurate
+recommendations. If no suitable recommendations are found in the context,
+respond with "I don't have any recommendations based on the provided characteristics."
+'''
+
+PROMPT_TEMPLATE = '''
+QUESTION: {question}
+
+CONTEXT:
+{context}
+'''.strip()
+
+
+class RAGBase:
+
+    def __init__(
+        self,
+        index,
+        llm_client,
+        instructions=INSTRUCTIONS,
+        prompt_template=PROMPT_TEMPLATE,
+        genre='',
+        model='gpt-5.4-mini'
+    ):
+        self.index = index
+        self.llm_client = llm_client
+        self.instructions = instructions
+        self.genre = genre
+        self.prompt_template = prompt_template
+        self.model = model
+
+    def search(self, query, num_results=5):
+        boost_dict = {'synopsis': 3.0, 'title': 1.5, 'studios': 1.0, 'genres': 1.0, 'type': 1.0}
+        filter_dict = {'genres': self.genre} if self.genre else {}
+
+        return self.index.search(
+            query,
+            num_results=num_results,
+            boost_dict=boost_dict,
+            filter_dict=filter_dict
+        )
+
+    def build_context(self, search_results):
+        lines = []
+
+        for doc in search_results:
+            lines.append('MAL ID: ' + str(doc['mal_id']))
+            lines.append('Genres: ' + doc['genres'])
+            lines.append('Type: ' + doc['type']) 
+            lines.append('Studios: ' + doc['studios'])
+            lines.append('Title: ' + doc['title'])
+            lines.append('Synopsis: ' + doc['synopsis'])
+            lines.append('')
+
+        return '\n'.join(lines).strip()
+
+    def build_prompt(self, query, search_results):
+        context = self.build_context(search_results)
+        return self.prompt_template.format(
+            question=query, context=context
+        )
+
+    def llm(self, prompt):
+        input_messages = [
+            {'role': 'developer', 'content': self.instructions},
+            {'role': 'user', 'content': prompt}
+        ]
+
+        response = self.llm_client.responses.create(
+            model=self.model,
+            input=input_messages
+        )
+
+        return response.output_text
+
+    def rag(self, query):
+        search_results = self.search(query)
+        prompt = self.build_prompt(query, search_results)
+        answer = self.llm(prompt)
+        return answer
